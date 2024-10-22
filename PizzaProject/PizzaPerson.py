c0bb65a1f8e6e489b706897bca604c98c6781764
zshamudeen <57739497+zshamudeen@users.noreply.github.com>
2024-10-22 19:14:17 -0400

    Update PizzaPerson.py

--- PizzaProject/PizzaPerson.py
+++ PizzaProject/PizzaPerson.py
@@ -4,7 +4,33 @@ import numpy as np
 import tkinter
 import customtkinter 
 
-customtkinter.set_appearance_mode("PizzaProject")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-app = customtkinter.CTk()
+root = customtkinter.CTk()
+
+root.geometry("500x350")
+
+def login():
+    print("test")
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady = 20, padx = 60, fill ="both", expand =True)
+
+label = customtkinter.CTkLabel(master = frame, text = "Login System", font = ("Roboto", 24))
+label.pack(pady = 12, padx = 10)
+
+entry1 = customtkinter.CTkEntry(master= frame, placeholder_text = "Username")
+entry1.pack (pady = 12, padx = 10)
+
+entry2 = customtkinter.CTkEntry(master= frame, placeholder_text = "Password")
+entry2.pack (pady = 12, padx = 10)
+
+button = customtkinter.CTkButton (master = frame, text = "Login", command = login)
+button.pack (pady = 12, padx = 10)
+
+checkbox = customtkinter.CTkCheckBox(master = frame, text = "Remember Me")
+checkbox.pack (pady = 12, padx = 10)
+
+root.mainloop()
