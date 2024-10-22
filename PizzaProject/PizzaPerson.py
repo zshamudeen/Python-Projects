1f43ff807b4fa4b07a51a601741dbcb9c4cf0f75
zshamudeen <57739497+zshamudeen@users.noreply.github.com>
2024-10-22 17:51:29 -0400

    Update PizzaPerson.py

--- PizzaProject/PizzaPerson.py
+++ PizzaProject/PizzaPerson.py
@@ -1,2 +1,10 @@
-print ("Hello")
 
+import PySimpleGUI as sg
+import numpy as np
+import tkinter
+import customtkinter 
+
+customtkinter.set_appearance_mode("PizzaProject")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
